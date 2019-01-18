c72988d0480fd2b6c0dfa9a787b146d084ab658d
atoris1192 <atoris1192@gmail.com>
2019-01-18 12:04:35 +0900

    first commit

---
+++ src/MyApp.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    p abc
+    p dddddddd
+</template>
+
+<style>
+</style>
+
+<script>
+  export default {
+    data() {
+      return({
+        title: 'Hello World! vue',
+      });
+    },
+  }
+</script>
